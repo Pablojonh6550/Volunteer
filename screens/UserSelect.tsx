5ae1576c81aa9e5b7fe921a0266f0e2ceaa730b8
Pablojonh6550 <pablojonh6550@gmail.com>
2023-05-15 20:02:31 -0300

    feat: user select

---
+++ screens/UserSelect.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { View, Text, TouchableOpacity } from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome";
+import { styles } from "../css/styles.js";
+
+export default function UserSelect({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity
+        style={styles.buttonPrev}
+        onPress={() => navigation.navigate("Login")}>
+        <Icon name="chevron-left" size={30} color="#374857" />
+      </TouchableOpacity>
+      <Text style={styles.title}>Escolha seu perfil</Text>
+      <View style={styles.profileContainer}>
+        <TouchableOpacity
+          style={styles.buttonPrimary}
+          onPress={() => navigation.navigate("RegisterUser")}>
+          <Text style={styles.buttonTitlePrimary}>Voluntário</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.buttonPrimary}
+          onPress={() => navigation.navigate("RegisterInstitution")}>
+          <Text style={styles.buttonTitlePrimary}>Instituição</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
