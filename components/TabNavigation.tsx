6ea9fe99a2f0260907201fbc4f094934e059713c
Pablojonh6550 <pablojonh6550@gmail.com>
2023-05-28 12:38:03 -0300

    feat: add route navigation

--- components/TabNavigation.tsx
+++ components/TabNavigation.tsx
@@ -3,12 +3,17 @@ import { View, Text, TouchableOpacity } from "react-native";
 import { styles } from "../css/styles";
 import Icon from "react-native-vector-icons/AntDesign";
 import Icons from "react-native-vector-icons/Octicons";
+import { useNavigation } from "@react-navigation/native";
+import { StackTypes } from "../App.js";
 
 export default function TabNavigation() {
+  const navigation = useNavigation<StackTypes>();
   return (
     <View style={styles.tabContainer}>
       <View style={styles.tabContent}>
-        <TouchableOpacity style={styles.tabButtonActive}>
+        <TouchableOpacity
+          style={styles.tabButtonActive}
+          onPress={() => navigation.navigate("Home")}>
           <Icon name="home" size={26} color="#fff" />
           <Text style={styles.tabTitle}>Home</Text>
         </TouchableOpacity>
@@ -16,7 +21,9 @@ export default function TabNavigation() {
           <Icons name="list-unordered" size={26} color="#fff" />
           <Text style={styles.tabTitle}>Tarefas</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.tabButton}>
+        <TouchableOpacity
+          style={styles.tabButton}
+          onPress={() => navigation.navigate("Perfil")}>
           <Icon name="user" size={26} color="#fff" />
           <Text style={styles.tabTitle}>Perfil</Text>
         </TouchableOpacity>
