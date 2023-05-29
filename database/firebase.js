5352169b73c577e00b24d6398a342062d74cfb27
Pablojonh6550 <pablojonh6550@gmail.com>
2023-05-29 17:52:59 -0300

    chove: firebase connection

---
+++ database/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBHcfxju6RA4l8zoHwHrCfWkrArfENyxeE",
+  authDomain: "app-login-firebase-3dcf1.firebaseapp.com",
+  projectId: "app-login-firebase-3dcf1",
+  storageBucket: "app-login-firebase-3dcf1.appspot.com",
+  messagingSenderId: "426482506074",
+  appId: "1:426482506074:web:eac21ebe68fcb4be273b63",
+};
+
+export default firebaseConfig;
