57b43ed595c67018211e690299a257a0a3a17ddf
Pablojonh6550 <pablojonh6550@gmail.com>
2023-05-29 17:52:42 -0300

    feat: add search icon

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,15 +1,18 @@
 import React from "react";
 import { View, Text, TextInput } from "react-native";
 import { styles } from "../css/styles";
-
+import Icon from "react-native-vector-icons/AntDesign";
 export default function Search() {
   return (
     <View style={styles.searchContainer}>
-      <TextInput
-        style={styles.inputSearch}
-        placeholder="Pesquisar..."
-        placeholderTextColor="#FFF"
-      />
+      <View style={styles.searchContent}>
+        <TextInput
+          style={styles.inputSearch}
+          placeholder="Pesquisar..."
+          placeholderTextColor="#FFF"
+        />
+        <Icon name="search1" size={26} color="#374857" />
+      </View>
     </View>
   );
 }
