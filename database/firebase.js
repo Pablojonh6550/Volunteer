c02e070aee999fe2fb8180ce04cf3ffaaff8974b
Pablojonh6550 <pablojonh6550@gmail.com>
2023-06-02 21:01:17 -0300

    feat: firebase config

--- database/firebase.js
+++ database/firebase.js
@@ -1,10 +1,33 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { collection, addDoc } from "firebase/firestore";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBHcfxju6RA4l8zoHwHrCfWkrArfENyxeE",
-  authDomain: "app-login-firebase-3dcf1.firebaseapp.com",
-  projectId: "app-login-firebase-3dcf1",
-  storageBucket: "app-login-firebase-3dcf1.appspot.com",
-  messagingSenderId: "426482506074",
-  appId: "1:426482506074:web:eac21ebe68fcb4be273b63",
+  apiKey: "AIzaSyC65GFobzhtY8X0u-TNN0L58jl0fH8S6Wo",
+  authDomain: "volunteer2-bc789.firebaseapp.com",
+  projectId: "volunteer2-bc789",
+  storageBucket: "volunteer2-bc789.appspot.com",
+  messagingSenderId: "599231924144",
+  appId: "1:599231924144:web:8fe9e890a518511a907875",
 };
 
-export default firebaseConfig;
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+const createUser = async (data, adress) => {
+  try {
+    const docRef = await addDoc(collection(db, "users"), {
+      name: data.name,
+      email: data.email,
+      phone: data.phone,
+      sex: data.sex,
+      birth: data.birth,
+      password: data.password,
+    });
+    console.log("Document written with ID: ", docRef.id);
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }
+};
+
+export { db, createUser };
