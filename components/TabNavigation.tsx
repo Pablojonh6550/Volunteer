ac7b3bd480c946ac749628360066f5e87c283134
Pablojonh6550 <pablojonh6550@gmail.com>
2023-06-03 21:33:15 -0300

    feat: new icons

--- components/TabNavigation.tsx
+++ components/TabNavigation.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { View, Text, TouchableOpacity } from "react-native";
 import { styles } from "../css/styles";
-import Icon from "react-native-vector-icons/AntDesign";
-import Icons from "react-native-vector-icons/Octicons";
+import Icon from "react-native-vector-icons/Ionicons";
+import Icons from "react-native-vector-icons/AntDesign";
 import { useNavigation } from "@react-navigation/native";
 import { StackTypes } from "../App.js";
 
@@ -14,17 +14,17 @@ export default function TabNavigation() {
         <TouchableOpacity
           style={styles.tabButtonActive}
           onPress={() => navigation.navigate("Home")}>
-          <Icon name="home" size={26} color="#fff" />
+          <Icon name="home-outline" size={26} color="#fff" />
           <Text style={styles.tabTitle}>Home</Text>
         </TouchableOpacity>
         <TouchableOpacity style={styles.tabButton}>
-          <Icons name="list-unordered" size={26} color="#fff" />
+          <Icon name="list-outline" size={26} color="#fff" />
           <Text style={styles.tabTitle}>Tarefas</Text>
         </TouchableOpacity>
         <TouchableOpacity
           style={styles.tabButton}
           onPress={() => navigation.navigate("Perfil")}>
-          <Icon name="user" size={26} color="#fff" />
+          <Icons name="user" size={26} color="#fff" />
           <Text style={styles.tabTitle}>Perfil</Text>
         </TouchableOpacity>
       </View>
