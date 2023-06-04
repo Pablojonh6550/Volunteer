30b41ec77f8bed46c15ed68e8fb891d56c75281b
Pablojonh6550 <pablojonh6550@gmail.com>
2023-06-03 21:31:48 -0300

    feat: auth validate

--- screens/Login.tsx
+++ screens/Login.tsx
@@ -1,19 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Image, Text, TextInput, TouchableOpacity } from "react-native";
 import CheckBox from "react-native-checkbox-component";
 import { styles } from "../css/styles.js";
 import { useNavigation } from "@react-navigation/native";
 import { StackTypes } from "../App.js";
-import firebase from "../database/firebase.js";
+import { auth } from "../database/firebase";
+import { signInWithEmailAndPassword } from "firebase/auth";
+interface Login {
+  email: string;
+  password: string;
+}
 
 export default function Login() {
   const navigation = useNavigation<StackTypes>();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async () => {
+    const data: Login = {
+      email: email,
+      password: password,
+    };
+
+    try {
+      signInWithEmailAndPassword(auth, data.email, data.password)
+        .then((userCredential) => {
+          console.log("Usuário autenticado:", userCredential.user);
+          navigation.navigate("Home");
+          setEmail("");
+          setPassword("");
+        })
+        .catch();
+    } catch (error) {
+      console.error("Error adding document: ", error);
+    }
+  };
+
   return (
     <View style={styles.container}>
       <Image source={require("../assets/logo.png")} style={styles.logo} />
       <View style={styles.inputContent}>
         <Text style={styles.label}>Digite seu email</Text>
-        <TextInput style={styles.input} placeholder="Usuário" />
+        <TextInput
+          style={styles.input}
+          placeholder="Usuário"
+          value={email}
+          onChange={(event) => setEmail(event.nativeEvent.text)}
+        />
       </View>
       <View style={styles.inputContent}>
         <Text style={styles.label}>Digite sua senha</Text>
@@ -21,6 +55,8 @@ export default function Login() {
           style={styles.input}
           placeholder="Senha"
           secureTextEntry={true}
+          value={password}
+          onChange={(event) => setPassword(event.nativeEvent.text)}
         />
       </View>
       <View style={styles.checkContent}>
@@ -36,7 +72,7 @@ export default function Login() {
           </TouchableOpacity>
         </View>
         <View style={styles.buttonContainer}>
-          <TouchableOpacity style={styles.buttonPrimary}>
+          <TouchableOpacity style={styles.buttonPrimary} onPress={handleLogin}>
             <Text style={styles.buttonTitlePrimary}>Entrar</Text>
           </TouchableOpacity>
         </View>
