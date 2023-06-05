aae6dc8195ed87a81c4dd6dc3e66862bd64bfdae
Pablojonh6550 <pablojonh6550@gmail.com>
2023-06-05 18:03:31 -0300

    feat: route params

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,30 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, ScrollView, Text } from "react-native";
 import { styles } from "../css/styles.js";
 import TabNavigation from "../components/TabNavigation";
 import Search from "../components/Search";
 import TaskCard from "../components/TaskCard";
+import { RouteProp, useRoute } from "@react-navigation/native";
+
+type RootStackParamList = {
+  Data: { email: string | null };
+};
+
+type DestinoScreenRouteProp = RouteProp<RootStackParamList, "Data">;
 
 export default function Home() {
+  const route = useRoute<DestinoScreenRouteProp>();
+  const { email } = route.params;
+  const userEmail = JSON.stringify(email);
+
   return (
     <View style={styles.homeContainer}>
       <View style={styles.taskHomeContainer}>
         <ScrollView>
-          <TaskCard />
-          <TaskCard />
-          <TaskCard />
-          <TaskCard />
-          <TaskCard />
-          <TaskCard />
-          <TaskCard />
-          <TaskCard />
-          <TaskCard />
-          <TaskCard />
-          <TaskCard />
+          <Text>{}</Text>
         </ScrollView>
       </View>
       <Search />
-      <TabNavigation />
+      <TabNavigation data={userEmail} />
     </View>
   );
 }
